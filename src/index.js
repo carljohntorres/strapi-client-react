1ce5c82c426d2967e2332ef1bd438bdfd2232f0b
Carl John <carljohntorres@gmail.com>
2022-11-17 16:28:29 +0800

    Strapi and React Integration

---
+++ src/index.js
@@ -0,0 +1,19 @@
+import React from "react";
+import { createRoot } from 'react-dom/client';
+import { BrowserRouter as Router } from "react-router-dom";
+import { ApolloProvider } from '@apollo/client';
+
+import App from "./containers/App";
+import client from "./utils/apolloClient";
+
+import "./index.css";
+
+const root = createRoot(document.getElementById('root'));
+
+root.render(
+    <Router>
+        <ApolloProvider client={client}>
+            <App />
+        </ApolloProvider>
+    </Router>
+);
